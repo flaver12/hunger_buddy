d68ff4ab36d27c915abdb03b172477d5e74e4d11
flaver <flaverkleiber@yahoo.de>
2015-07-16 23:10:59 +0200

    First function and some macros

--- jtog_hunger_buddy_scripts/config.cpp
+++ jtog_hunger_buddy_scripts/config.cpp
@@ -9,4 +9,25 @@
  *
  */
  
- class CfgPatches {};
+//Patches class
+ class CfgPatches {
+
+ 	//Config for the addon
+	class jtog_hunger_buddy_scripts {
+		units[] = {};
+		requiredVersion = 0.60;
+		requiredAddons[] = {CBA_MAIN,"a3_modules_f","jtog_hunger_buddy_items"};
+		version 	= "0.0.4";
+		author[] 	= {"flaver"};
+		authorUrl 	= "https://github.com/flaver12";
+	};
+
+};
+
+//Functions wrapper
+class CfgFunctions {
+	
+	class jtog_hunger_buddy_scripts {
+		#include <\jtog_hunger_buddy_scripts\CfgFunctions.cpp>
+	};
+};
